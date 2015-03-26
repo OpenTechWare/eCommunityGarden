c7dab62cb29fd4f75e24e152cf40e0daa741e0f0
CompulsiveCoder <compulsivecoder@gmail.com>
2015-03-26 14:05:26 +1100

    Improved help comments and reorganized code

--- src/Sketches/SensorNode/SensorNode.ino
+++ src/Sketches/SensorNode/SensorNode.ino
@@ -9,74 +9,78 @@ http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
   
   1) Add the sensor number (this must also be defined in the web.config file of the web application)
 
-  2) Increase the total sensors number
+  2) Set the total sensors number
   
   3) Define the input pin of the sensor
       
   4) Get the sensor value during each loop
-    Example:
-      double [name]Value = map(analogRead([name]SensorPin), 0, 1023, 0, 100);
-      
+  
   6) Add the sensor value to the sensorValues array
-    Example:
-      sensorValues[3] = [name]Value;
+  
+  Note: Only a single sensor value is sent to the hub during each loop.
+  while this seems counter-intuitive it allows virtually any number of sensors to be added.
 */
 
-/*-----( Import needed libraries )-----*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-/*-----( Declare Constants and Pin Numbers )-----*/
+
+// nRF pins
 #define CE_PIN   9
 #define CSN_PIN 10
 
-// NOTE: the "LL" at the end of the constant is "LongLong" type
-const uint64_t pipe = 0xE8E8F0F0E1LL; // Define the transmit pipe
+// ========== Start Modification Area
 
-/*-----( Declare objects )-----*/
-RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
-
-/*-----( Declare Variables )-----*/
-// Create an array to hold the data to be sent. (Don't modify this.)
-int data[5];
+// !! Modify the following code to set a unique Id of the device
 
 // Define the Id of the sensor node. This Id must be unique.
 int id[] = {1, 1, 2};
 
-// The current position in the list of sensors/values. (Don't modify this.)
-int sensorPosition = 0;
+// !! Modify the following code to change the nRF pipe/network to communicate on
 
+// Define the nRF transmit pipe
+// Devices using the same pipe will be able to communicate with each other.
+// NOTE: the "LL" at the end of the constant is "LongLong" type
+const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 // !! Modify the following code when adding add new sensors
-// ========== Start
 
-// Define the sensor numbers.
-// IMPORTANT: These must match those in the web.config file of the web application.
+// Define the sensor numbers
 const int lightSensorNumber = 1;
 const int soilMoistureSensorNumber = 2;
 const int temperatureSensorNumber = 3;
-// 1) Add more sensor numbers here. Make sure you add it to the web.config file of the web application.
+// 1) Add more sensor numbers here.
+//    Define the sensor number in the web.config file of the web application
 // const int [name]SensorNumber = 4;
 
-// 2) Increase this number for each new sensor.
+// 2) Set this to the total number of sensors
 const int totalSensors = 3;
 
-// Define the sensor numbers array.
-// IMPORTANT: The position of these must match the position of the sensor values in the sensorValues array.
+// Define the sensor input pins
+#define lightSensorPin A4
+#define soilMoistureSensorPin A5
+#define temperatureSensorPin A3
+// 3) Add more sensors pin declarations here
+// #define [name]SensorPin AX
+
+// ========== End Modification Area
+
+// Define the sensor numbers array
 int sensorNumbers[totalSensors];
 
-// Declare an array for the sensor values.
-// IMPORTANT: The position of each value must match the position of the corresponding sensor numbers in the sensorNumbers array.
+// Declare an array for the sensor values
 int sensorValues[totalSensors];
 
-// Define the sensor input pins
-const int lightSensorPin = A4;
-const int soilMoistureSensorPin = A5;
-const int temperatureSensorPin = A3;
-// 3) Add more sensors pin declarations here
-// const int [name]SensorPin = AX;
 
-// ========== End
+// Create nRF radio object
+RF24 radio(CE_PIN, CSN_PIN);
+
+// Create an array to hold the data to be sent. (Don't modify this.)
+int data[5];
+
+
+// The current position in the list of sensors/values. It will increment automatically during each loop. (Don't modify this.)
+int sensorPosition = 0;
 
 void setup()
 {
@@ -88,8 +92,8 @@ void setup()
 
 void loop()
 {
+  // ========== Start Modification Area
   // !! Modify the following code when adding add new sensors
-  // ========== Start
   
   // Get the sensor values...
   
@@ -112,7 +116,7 @@ void loop()
   // Set the sensor values...
   
   // Light sensor
-  sensorNumbers[0] = lightSensorNumber
+  sensorNumbers[0] = lightSensorNumber;
   sensorValues[0] = lightValue;
   
   // Soil moisture sensor
@@ -127,7 +131,7 @@ void loop()
   // sensorNumbers[3] = [name]SensorNumber;
   // sensorValues[3] = [name]Value;
   
-  // ========== End
+  // ========== End Modification Area
   // !! DO NOT modify the code below when adding sensors.
   
   // Add the Id and the sensor number/value for the current position.
