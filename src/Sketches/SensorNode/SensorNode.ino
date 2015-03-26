87e4ad80c1f461206726e57024d29d4aa6fd0463
CompulsiveCoder <compulsivecoder@gmail.com>
2015-03-26 13:51:55 +1100

    Simplified the sensor node code so it's easier to add sensors

--- src/Sketches/SensorNode/SensorNode.ino
+++ src/Sketches/SensorNode/SensorNode.ino
@@ -8,25 +8,12 @@ http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
   (The following numbers are used throughout the code to indicate where to modify it.)
   
   1) Add the sensor number (this must also be defined in the web.config file of the web application)
-    Example:
-      const int [name]SensorNumber = 4;
-      
-  2) Include the sensor number in the sensorNumbers array
-    Example:
-      int sensorNumbers[] = {
-        ..,
-        [name]SensorNumber
-      };
+
+  2) Increase the total sensors number
   
-  3) Increase the total sensors number
-    Example:
-      int totalSensors = 4;
+  3) Define the input pin of the sensor
       
-  4) Define the sensor input pin
-    Example:
-      const int [name]SensorPin = A6;
-      
-  5) Get the sensor value during each loop
+  4) Get the sensor value during each loop
     Example:
       double [name]Value = map(analogRead([name]SensorPin), 0, 1023, 0, 100);
       
@@ -59,32 +46,37 @@ int id[] = {1, 1, 2};
 // The current position in the list of sensors/values. (Don't modify this.)
 int sensorPosition = 0;
 
+
+// !! Modify the following code when adding add new sensors
+// ========== Start
+
 // Define the sensor numbers.
 // IMPORTANT: These must match those in the web.config file of the web application.
 const int lightSensorNumber = 1;
 const int soilMoistureSensorNumber = 2;
 const int temperatureSensorNumber = 3;
-// 1) Add more sensor numbers here
+// 1) Add more sensor numbers here. Make sure you add it to the web.config file of the web application.
+// const int [name]SensorNumber = 4;
+
+// 2) Increase this number for each new sensor.
+const int totalSensors = 3;
 
 // Define the sensor numbers array.
 // IMPORTANT: The position of these must match the position of the sensor values in the sensorValues array.
-int sensorNumbers[] = {
-  lightSensorNumber,
-  soilMoistureSensorNumber,
-  temperatureSensorNumber//,
-  // 2) Add more sensor numbers here, but ensure they're defined in the web.config file as well.
-  };
+int sensorNumbers[totalSensors];
 
 // Declare an array for the sensor values.
 // IMPORTANT: The position of each value must match the position of the corresponding sensor numbers in the sensorNumbers array.
-int totalSensors = 3; // 3) Increase this number to add new sensors.
 int sensorValues[totalSensors];
 
 // Define the sensor input pins
 const int lightSensorPin = A4;
 const int soilMoistureSensorPin = A5;
 const int temperatureSensorPin = A3;
-// 4) Add more sensors pin declarations here
+// 3) Add more sensors pin declarations here
+// const int [name]SensorPin = AX;
+
+// ========== End
 
 void setup()
 {
@@ -96,6 +88,10 @@ void setup()
 
 void loop()
 {
+  // !! Modify the following code when adding add new sensors
+  // ========== Start
+  
+  // Get the sensor values...
   
   // Get the light sensor value
   double lightValue = map(analogRead(lightSensorPin), 0, 1023, 0, 100);
@@ -109,13 +105,30 @@ void loop()
   temperatureValue = 1 / (0.001129148 + (0.000234125 * temperatureValue) + (0.0000000876741 * temperatureValue * temperatureValue * temperatureValue));
   temperatureValue = temperatureValue - 273.15;
   
-  // 5) Get the value of any additional sensors here.
+  // 4) Get the value of additional sensors here
+  // double [name]Value = analogRead([name]SensorPin);
+  // double [name]Value = map(analogRead([name]SensorPin), 0, 1023, 0, 100); // Converts analog input of 0-1023 to 0-100 (ie. percentage).
+ 
+  // Set the sensor values...
   
-  // Set the sensor values. Each value must be in the same position as the corresponding label
+  // Light sensor
+  sensorNumbers[0] = lightSensorNumber
   sensorValues[0] = lightValue;
+  
+  // Soil moisture sensor
+  sensorNumbers[1] = soilMoistureSensorNumber;
   sensorValues[1] = soilMoistureValue;
+  
+  // Temperature sensor
+  sensorNumbers[2] = temperatureSensorNumber;
   sensorValues[2] = temperatureValue;
-  // 6) Add additional sensor values here. Ensure that each value has the corresponding sensor number in sensorNumbers array.
+  
+  // 5) Set additional sensor values here
+  // sensorNumbers[3] = [name]SensorNumber;
+  // sensorValues[3] = [name]Value;
+  
+  // ========== End
+  // !! DO NOT modify the code below when adding sensors.
   
   // Add the Id and the sensor number/value for the current position.
   // Only one sensor value is sent per time.
