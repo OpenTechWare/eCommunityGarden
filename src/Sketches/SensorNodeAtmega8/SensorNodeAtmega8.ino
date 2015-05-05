443507a83f3036edb649c9a41a73123bb8bc11a8
CompulsiveCoder <compulsivecoder@gmail.com>
2015-05-05 10:57:42 +1000

    Added SensorNodeAtmega8 sketch. It's a smaller sketch which fits on an atmega8 chip

---
+++ src/Sketches/SensorNodeAtmega8/SensorNodeAtmega8.ino
@@ -0,0 +1,336 @@
+/*
+Code based on examples provided by:
+ http://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-HowTo
+ */
+
+/*
+  How to add sensors...
+ (The following numbers are used throughout the code to indicate where to modify it.)
+ 
+ 1) Add the sensor number (this must also be defined in the web.config file of the web application)
+ 
+ 2) Set the total sensors number
+ 
+ 3) Define the input pin of the sensor
+ 
+ 4) Get the sensor value during each loop
+ 
+ 6) Add the sensor value to the sVals array
+ 
+ Note: Only a single sensor value is sent to the hub during each loop.
+ while this seems counter-intuitive it allows virtually any number of sensors to be added.
+ */
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <EEPROM.h>
+
+// Buttons
+#include <Button.h>
+
+// LCD Display
+#include <Wire.h> 
+//#include <LiquidCrystal_I2C.h>
+
+//LiquidCrystal_I2C lcd(0x3F,16,2);
+
+
+// nRF pins
+#define CE_PIN   9
+#define CSN_PIN 10
+
+// ========== Start Modification Area
+
+// !! Modify the following code to set a unique Id of the device
+
+// Define the Id of the sensor node. This Id must be unique.
+int id[] = {
+  1, 1, 2};
+
+// !! Modify the following code to change the nRF pipe/network to communicate on
+
+// Define the nRF transmit pipe
+// Devices using the same pipe will be able to communicate with each other.
+// NOTE: the "LL" at the end of the constant is "LongLong" type
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+
+// !! Modify the following code when adding add new sensors
+
+// Define the sensor codes
+#define ltCode 1
+#define mstCode 2
+#define tmpCode 3
+// 1) Add more sensor codes here.
+//    Define the sensor code in the web.config file of the web application
+// const int [name] = 4;
+
+// 2) Set this to the total number of sensors
+const int totalSensors = 3;
+
+// Define the sensor input pins
+#define ltPin A0
+#define tmpPin A1
+#define mstPin A2
+// 3) Add more sensors pin declarations here
+// #define [name]SensorPin AX
+
+// ========== End Modification Area
+
+// Define the sensor numbers array
+int sNums[totalSensors];
+
+// Define the sensor codes array
+int sCodes[totalSensors];
+
+// Declare an array for the sensor values
+int sVals[totalSensors];
+
+// Declare an array for the sensor letters
+char* sLets[totalSensors];
+
+
+// Create nRF radio object
+RF24 radio(CE_PIN, CSN_PIN);
+
+// Create an array to hold the data to be sent. (Don't modify this.)
+int data[6];
+
+
+// The current position in the list of sensors/values. It will increment automatically during each loop. (Don't modify this.)
+int sensorPosition = 0;
+
+
+/*Button idBtn = Button(2, &idHandler);
+Button idUpBtn = Button(3, &idUpHandler);
+Button idDownBtn = Button(4, &idDownHandler);
+
+//Keep pointers to all the buttons in an array for simpler handling of many buttons
+const int buttonAmount = 3;
+Button* buttons[buttonAmount] = {
+  &idBtn,
+  &idUpBtn,
+  &idDownBtn
+};*/
+
+int editIdPosition = -1;
+
+
+void setup()
+{
+  //Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  
+  // Start LCD display
+ // lcd.init();    
+  //lcd.backlight();
+
+  loadId();
+  //refreshLCD();
+}
+
+
+void loop()
+{
+  //detectButton();
+
+  // ========== Start Modification Area
+  // !! Modify the following code when adding add new sensors
+
+  // Get the sensor values...
+
+  // Get the light sensor value
+  double ltVal = map(analogRead(ltPin), 0, 1023, 0, 100);
+
+  // Get the soil moisture sensor value
+  double mstVal = map(analogRead(mstPin), 1023, 0, 0, 100);
+
+  // Get the temperature sensor value
+  int tmpVal = getCelsius(analogRead(tmpPin));
+
+  // 4) Get the value of additional sensors here
+  // double [name]Value = analogRead([name]SensorPin);
+  // double [name]Value = map(analogRead([name]SensorPin), 0, 1023, 0, 100); // Converts analog input of 0-1023 to 0-100 (ie. percentage).
+
+  // Set the sensor values...
+
+  // Light sensor
+  sNums[0] = 1;
+  sLets[0] = "L";
+  sCodes[0] = ltCode;
+  sVals[0] = ltVal;
+
+  // Temperature sensor
+  sNums[1] = 2;
+  sLets[1] = "T";
+  sCodes[1] = tmpCode;
+  sVals[1] = tmpVal;
+  
+  // Soil moisture sensor
+  sNums[2] = 3;
+  sLets[2] = "M";
+  sCodes[2] = mstCode;
+  sVals[2] = mstVal;
+
+
+  // 5) Set additional sensor values here
+  // sNums[3] = 4;
+  // sLets[3] = "[letter]";
+  // sCodes[3] = [name]Num;
+  // sVals[3] = [name]Val;
+
+  // ========== End Modification Area
+  // !! DO NOT modify the code below when adding sensors.
+
+  // Add the Id and the sensor code/value for the current position.
+  // Only one sensor value is sent per time.
+  data[0] = id[0];
+  data[1] = id[1];
+  data[2] = id[2];
+  data[3] = sNums[sensorPosition];
+  data[4] = sCodes[sensorPosition];
+  data[5] = sVals[sensorPosition];
+
+  // Increment the sensor position to loop through each sensor value
+  sensorPosition += 1;
+  if (sensorPosition >= totalSensors)
+    sensorPosition = 0;
+    
+  /*Serial.print("Id:");
+  Serial.print(id[0]);
+  Serial.print(".");
+  Serial.print(id[0]);
+  Serial.print(".");
+  Serial.print(id[0]);
+  Serial.print(";");
+  Serial.print("S:");
+  Serial.print(data[3]);
+  Serial.print(";V:");
+  Serial.print(data[4]);
+  Serial.println(";");
+*/
+  radio.write( data, sizeof(data) );
+  
+ // lazyRefreshLCD();
+}
+
+/*void detectButton()
+{
+  for (int i = 0; i < buttonAmount; i++){
+    buttons[i]->read();
+  }
+}
+
+void idHandler(Button* button)
+{
+  if (button->pressed){
+    editIdPosition++;
+    if (editIdPosition > 2)
+      editIdPosition = -1;
+  }
+
+  //refreshLCD();
+}
+
+void idUpHandler(Button* button)
+{
+  if (button->pressed){
+    id[editIdPosition]++;
+    if (id[editIdPosition] > 127)
+      id[editIdPosition] = 0;
+  }
+
+  saveId();
+  //refreshLCD();
+}
+void idDownHandler(Button* button)
+{
+  if (button->pressed){
+    id[editIdPosition]--;
+    if (id[editIdPosition] < 1)
+      id[editIdPosition] = 127;
+  }
+
+  saveId();
+  //refreshLCD();
+}*/
+
+//void refreshLCD()
+//{
+  /*lcd.clear();
+  lcd.print("Id: ");
+
+  for (int i = 0; i < 3; i++)
+  {
+    if (editIdPosition == i)
+      lcd.print("[");
+    lcd.print(id[i]);
+    if (editIdPosition == i)
+      lcd.print("]");
+
+    if (i < 2)
+      lcd.print(".");
+  }
+
+  lcd.setCursor(0, 1);
+
+  for (int i = 0; i < totalSensors; i++)
+  {
+    lcd.print(sLets[i]);
+    lcd.print(":");
+    lcd.print(sVals[i]);
+    lcd.print(" ");
+  }*/
+//}
+
+int skipRefreshCount = 0;
+
+/*void lazyRefreshLCD()
+{
+  skipRefreshCount++;
+
+  if (skipRefreshCount > 20)
+  {
+    refreshLCD();
+    skipRefreshCount = 0;
+  }
+}*/
+
+void saveId()
+{
+  for (int i = 0; i < 3; i++)
+  {
+    if (EEPROM.read(i) != id[i])
+      EEPROM.write(i, id[i]);
+  }
+}
+
+void loadId()
+{
+  int foundId[3];
+  //Serial.print("EEPROM ID:");  
+  for (int i = 0; i < 3; i++)
+  {
+    foundId[i] = EEPROM.read(i);
+
+   // Serial.print(foundId[i]);
+
+  //  if (i < 2)  
+  //    Serial.print(".");
+
+    if (foundId[i] > 0 && foundId[i] < 255)
+      id[i] = foundId[i];
+  }
+
+
+}
+
+double getCelsius(int analogInput) {
+ double temp;
+ temp = log(10000.0*((1024.0/analogInput-1))); 
+//         =log(10000.0/(1024.0/analogInput-1)) // for pull-up configuration
+ temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * temp * temp ))* temp );
+ temp = temp - 273.15;            // Convert Kelvin to Celcius
+ return temp;
+}
